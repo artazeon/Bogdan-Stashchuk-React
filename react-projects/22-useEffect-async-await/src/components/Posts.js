abb818a63e2fd42ffa1fd7c2d6f8fc58def7ed35
artazeon <artazeon@gmail.com>
2023-05-03 15:00:18 +0300

    Раздел 26 Проект с async await в useEffect

--- react-projects/22-useEffect-async-await/src/components/Posts.js
+++ react-projects/22-useEffect-async-await/src/components/Posts.js
@@ -6,16 +6,44 @@ function Posts() {
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(true)
 
+  const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((posts) => {
-        console.log(posts)
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL)
+        const posts = await res.json()
         setPosts(posts)
-      })
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false))
+      } catch (error) {
+        setError(error.message)
+      }
+      setIsLoading(false)
+    }
+
+    fetchData()
   }, [])
+
+  // Возвращает промис, ошибка в консоли, нужно обернуть в функцию
+  // useEffect(async () => {
+  //   try {
+  //     const res = await fetch(API_URL)
+  //     const posts = await res.json()
+  //     setPosts(posts)
+  //   } catch (error) {
+  //     setError(error.message)
+  //   }
+  //   setIsLoading(false)
+  // }, [])
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((response) => response.json())
+  //     .then((posts) => {
+  //       setPosts(posts)
+  //     })
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false))
+  // }, [])
 
   if (error) {
     return <h1>Error: {error}</h1>
